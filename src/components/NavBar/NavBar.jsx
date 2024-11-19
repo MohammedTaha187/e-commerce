ccea5d6196aa7b7479377580676495145c6e067e
mohammed taha <ant833292@gmail.com>
2024-11-19 20:14:50 +0200

    add: update data

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,33 +9,33 @@ function NavBar() {
   return <>
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark text-white">
       <div className="container">
-        <Link className="nav-link" to={'/home'}><i className="fa-solid fa-cart-shopping" style={{color: '#95a0b1'}} />Shop </Link>
+        <Link className="nav-link" to={'/e-commerce/home'}><i className="fa-solid fa-cart-shopping" style={{color: '#95a0b1'}} />Shop </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to={'/home'}>Home</Link>
+              <Link className="nav-link" to={'/e-commerce/home'}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/shop'}>Shop</Link>
+              <Link className="nav-link" to={'/e-commerce/shop'}>Shop</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/about'}>About</Link>
+              <Link className="nav-link" to={'/e-commerce/about'}>About</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/contact'}>Contact</Link>
+              <Link className="nav-link" to={'/e-commerce/contact'}>Contact</Link>
             </li>
           </ul>
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to={'/register'}>
+              <Link className="nav-link" to={'/e-commerce/register'}>
                 <i className="fa-regular fa-registered" style={{color: '#8893a5'}} />
               </Link>
             </li>
             <li className="nav-item position-relative">
-              <Link className="nav-link" to={'/cart'}>
+              <Link className="nav-link" to={'/e-commerce/cart'}>
               <i className="fa-solid fa-cart-shopping"></i>
                 {cartDetails?.length > 0 && (
                   <span className="badge bg-danger position-absolute top-0 start-100 translate-middle">
@@ -44,7 +44,6 @@ function NavBar() {
                 )}
               </Link>
             </li>
-            
           </ul>
         </div>
       </div>
