c68dee50588a87577f9b33caae703cc81e4b6e5e
mohammed taha <ant833292@gmail.com>
2024-11-19 20:53:09 +0200

    add: update data

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,26 +16,26 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to={'/home'}>Home</Link>
+              <Link className="nav-link" to={''}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/shop'}>Shop</Link>
+              <Link className="nav-link" to={''}>Shop</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/about'}>About</Link>
+              <Link className="nav-link" to={''}>About</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/contact'}>Contact</Link>
+              <Link className="nav-link" to={''}>Contact</Link>
             </li>
           </ul>
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to={'/register'}>
+              <Link className="nav-link" to={''}>
                 <i className="fa-regular fa-registered" style={{color: '#8893a5'}} />
               </Link>
             </li>
             <li className="nav-item position-relative">
-              <Link className="nav-link" to={'/cart'}>
+              <Link className="nav-link" to={''}>
               <i className="fa-solid fa-cart-shopping"></i>
                 {cartDetails?.length > 0 && (
                   <span className="badge bg-danger position-absolute top-0 start-100 translate-middle">
